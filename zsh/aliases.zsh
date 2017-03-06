887929ccdc489ab088433b8df2f20a5a1c800861
Christoph Neyer <christoph.neyer@gmail.com>
2017-03-06 14:39:16 -0800

    More updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,6 @@ alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
 
-alias h='history'
+alias h='fc -li 1'
 
 alias flushdns='dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
